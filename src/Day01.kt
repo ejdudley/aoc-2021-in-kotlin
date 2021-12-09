77c9e33d7d909d831237eade3ef3592ccbe7aafa
ed <dudleyej@gmail.com>
2021-12-09 20:05:58 +0000

    First version

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,17 @@
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+
+        val n = input.map { it.toInt() }
+            .reduceIndexed { index, acc, _ -> acc + if (input[index] > input[index - 1]) 1 else 0 }
+
+        return n - input[0].toInt()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
-    println(part2(input))
+
 }
