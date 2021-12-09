1f57c1c8eb5f922b146dc6e114727c8af2cf1f15
ed <dudleyej@gmail.com>
2021-12-09 20:32:48 +0000

    Day1 both parts

--- src/Day01.kt
+++ src/Day01.kt
@@ -12,10 +12,32 @@ fun main() {
 
     }
 
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 7)
+    fun part2(input: List<String>): Int {
 
+        val inputInts = input.map { it -> it.toInt() }
+
+        val listLen = input.size - 2
+        var prevSum = inputInts.take(3).sum()
+
+        var count = 0
+
+        for (i in 1 until listLen) {
+            val currSum = prevSum - inputInts[i - 1] + inputInts[i + 2]
+            if (currSum > prevSum) count++
+            prevSum = currSum
+        }
+
+        return count
+
+    }
+
+    val testInput = readInput("Day01_test")
     val input = readInput("Day01")
+
+    check(part1(testInput) == 7)
     println(part1(input))
+
+    check(part2(testInput) == 5)
+    println(part2(input))
 
 }
